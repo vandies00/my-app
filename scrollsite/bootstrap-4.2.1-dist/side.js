8b5032c78a5c5dd80799726d3fcbaa9f1693e6b5
Lisa Vandenhoeck <lisavandenhoeck@Lisas-MBP-2.fritz.box>
2019-02-09 23:43:39 +0100

    bootstrap and fullpage
    exercise

---
+++ scrollsite/bootstrap-4.2.1-dist/side.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+    scrollHorizontally: true,
+    navigation: true,
+    // slidesNavigation: true,
+   
+});
+
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,19 @@
-import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
 import React from 'react'
+import Link from 'gatsby-link'
+import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+const Header = ({siteTitle}) => (
+
+  <div className="Header">
+  <div className="HeaderGroup">
+  <Link to="/"><img src={require('../images/logo_msc.png')} width="38"/></Link>
+  <Link to="/courses">Courses</Link>
+  <Link to="/downloads">Downloads</Link>
+  <Link to="/workshops">Workshops</Link>
+  <Link to="/buy"><button>Buy</button></Link>
+
   </div>
+  </div>  
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: '',
-}
 
 export default Header
